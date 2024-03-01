6177372ecea1a81bcca9373c9a389008e43dbc1e
LauraSabrina90 <sanchezlaurasabrina@hotmail.com>
2024-03-01 02:53:24 +0100

    stucked endpoints

--- src/config/myConnection.js
+++ src/config/myConnection.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDb = () => {
-    const user = 'LauraSabrina';
-    const password = 'mongoMongo';
-    const dbName = 'bills';
+    const user = process.env.DB_USER;
+    const password = process.env.DB_PASSWORD;
+    const dbName = process.env.DB_NAME;
 
     const uri = `mongodb+srv://${user}:${password}@modulo-4.zqnxcir.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Modulo-4`;
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDb = require('./config/myConnection');
+const Expenses = require('./models/expenses.models')
 
 
 //ARRANCAR SERVIDOR
@@ -11,7 +12,7 @@ const myServer = express();
 
 //CONFIGURAR SERVIDOR
 myServer.use(cors());
-myServer.use(express.json( { limit: '25mb ' }));
+myServer.use(express.json());
 
 //CONEXIÓN BD
 connectDb();
@@ -23,4 +24,68 @@ myServer.listen(myPort, () => {
 });
 
 
+//para los endpoints, con mongoDB hay que definir estructuras de datos por cada una de las colecciones a las que quiero acceder --> modelos
 
+//Insertar un registro en su entidad principal.
+    myServer.post('/addExpenses', async (req, res) => {
+        console.log(req.body);
+        try {
+            const newExpense = await Expense.create(req.body);
+            console.log(newExpense.month);
+            res.json(newExpense);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    });
+    myServer.post('/addExpenses', async (req, res) => {
+    console.log(req.body);
+    try {
+        const newExpense = await Expense.create(req.body);
+        console.log(newExpense.month);
+        res.json(newExpense);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+//Leer/Listar todos los registros existentes.
+myServer.get('/getExpenses', async (req, res) => {
+    const result = await Expenses.find();
+    res.json(result);
+    console.log(result);
+
+});
+
+
+//Leer registros filtrado por el campo de tu interés.
+
+
+//Actualizar un registro existente.
+ myServer.put('/updateExpenses', (req, res) => {
+  Person.findOneAndUpdate(
+    { name: 'pedro' },
+    { $set: { name: 'Naomi' } },
+    { new: true }
+  )
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//Eliminar un registro existente.
+
+myServer.delete('/delete', (req, res) => {
+    Expenses.deleteOne({ 'luxuries' : '50' })
+      .then((data) => {
+        console.log('Delete Docs : ', docs);
+        res.json(docs);
+      })
+      .catch((err) => {
+        console.log('Ha ocurrido un error en la información' + err);
+      });
+  });
